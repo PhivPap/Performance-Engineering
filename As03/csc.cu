9f26163b216f8a03c771bfd4392a445f1592fa8c
PMMS course <pmms2221@fs0.cm.cluster>
2022-05-23 15:45:23 +0200

    csc version (produces incorrect results) will only fix if there is ample time left

--- As03/csc.cu
+++ As03/csc.cu
@@ -33,67 +33,64 @@ void checkCudaCall(cudaError_t result, uint32_t line)
     }
 }
 
-__global__ void csr(const int *A_rows, const int *A_cols_idx, const float *A_values, const float *B, float *C) {
+__global__ void csc(const int *A_cols, const int *A_rows_idx, const float *A_values, const float *B, float *C) {
     int i,j;
-    int row_start, row_end;
+    int col_start, col_end; 
 
     i = blockIdx.x;  
 
-        float tmp = 0.0f;
-        row_start = A_rows[i];
-        row_end = A_rows[i + 1];
+        col_start = A_cols[i];
+        col_end = A_cols[i + 1];
         
-        for (j = row_start; j < row_end; j++) {
-            tmp += A_values[j] * B[A_cols_idx[j]];
+        for (j = col_start; j < col_end; j++) {
+            C[A_rows_idx[j]] += A_values[j] * B[i];
         }
-        
-        C[i] = tmp;
 }
 
-void gpu_memory_init(int m, int n, int nzA, int *A_rows, int *A_cols_idx, float *A_values, float *B, float *C,
-    int **d_A_rows, int **d_A_cols_idx, float **d_A_values, float **d_B, float **d_C)
+void gpu_memory_init(int m, int n, int nzA, int *A_cols, int *A_rows_idx, float *A_values, float *B, float *C,
+    int **d_A_cols, int **d_A_rows_idx, float **d_A_values, float **d_B, float **d_C)
 {
-    const uint32_t A_rows_size = sizeof(int) * (m + 1);
-    const uint32_t A_cols_idx_size = sizeof(int) * nzA;
+    const uint32_t A_cols_size = sizeof(int) * (n + 1);
+    const uint32_t A_rows_idx_size = sizeof(int) * nzA;
     const uint32_t A_values_size = sizeof(float) * nzA;
     const uint32_t B_size = sizeof(float) * n;
     const uint32_t C_size = sizeof(float) * m; 
 
-    check(cudaMalloc(d_A_rows, A_rows_size));
-    check(cudaMalloc(d_A_cols_idx, A_cols_idx_size));
+    check(cudaMalloc(d_A_cols, A_cols_size));
+    check(cudaMalloc(d_A_rows_idx, A_rows_idx_size));
     check(cudaMalloc(d_A_values, A_values_size));
     check(cudaMalloc(d_B, B_size));
     check(cudaMalloc(d_C, C_size));
 
-    check(cudaMemcpy(*d_A_rows, A_rows, A_rows_size, cudaMemcpyHostToDevice));
-    check(cudaMemcpy(*d_A_cols_idx, A_cols_idx, A_cols_idx_size, cudaMemcpyHostToDevice));
+    check(cudaMemcpy(*d_A_cols, A_cols, A_cols_size, cudaMemcpyHostToDevice));
+    check(cudaMemcpy(*d_A_rows_idx, A_rows_idx, A_rows_idx_size, cudaMemcpyHostToDevice));
     check(cudaMemcpy(*d_A_values, A_values, A_values_size, cudaMemcpyHostToDevice));
     check(cudaMemcpy(*d_B, B, B_size, cudaMemcpyHostToDevice));
     check(cudaMemcpy(*d_C, C, C_size, cudaMemcpyHostToDevice));
 }
 
-void gpu_memory_free(int *d_A_rows, int *d_A_cols_idx, float *d_A_values, float *d_B, float *d_C){
-    check(cudaFree(d_A_rows));
-    check(cudaFree(d_A_cols_idx));
+void gpu_memory_free(int *d_A_cols, int *d_A_rows_idx, float *d_A_values, float *d_B, float *d_C){
+    check(cudaFree(d_A_cols));
+    check(cudaFree(d_A_rows_idx));
     check(cudaFree(d_A_values));
     check(cudaFree(d_B));
     check(cudaFree(d_C));
 }
 
-void csr_spmv(int m, int n, int nzA, int *A_rows, int *A_cols_idx, float *A_values, float *B, float *C) {
+void csc_spmv(int m, int n, int nzA, int *A_cols, int *A_rows_idx, float *A_values, float *B, float *C) {
     int r;
 
-    int *d_A_rows, *d_A_cols_idx;
+    int *d_A_cols, *d_A_rows_idx;
     float *d_A_values;
     float *d_B;
     float *d_C;
 
-    gpu_memory_init(m, n, nzA, A_rows, A_cols_idx, A_values, B, C, &d_A_rows, &d_A_cols_idx, &d_A_values, &d_B, &d_C);
+    gpu_memory_init(m, n, nzA, A_cols, A_rows_idx, A_values, B, C, &d_A_cols, &d_A_rows_idx, &d_A_values, &d_B, &d_C);
 
     const auto start = std::chrono::system_clock::now();
     for (r=0; r<REP; r++) 
     {
-        csr<<<m, 1>>>(d_A_rows, d_A_cols_idx, d_A_values, d_B, d_C);
+        csc<<<n, 1>>>(d_A_cols, d_A_rows_idx, d_A_values, d_B, d_C);
         cudaDeviceSynchronize();
     }
     const auto end = std::chrono::system_clock::now();
@@ -105,7 +102,7 @@ void csr_spmv(int m, int n, int nzA, int *A_rows, int *A_cols_idx, float *A_valu
     printf("Seconds: %f\n", avg_execution_time);
     printf("REP: %i\n", REP);
     
-    gpu_memory_free(d_A_rows, d_A_cols_idx, d_A_values, d_B, d_C);
+    gpu_memory_free(d_A_cols, d_A_rows_idx, d_A_values, d_B, d_C);
 }
 
 /* 
@@ -229,39 +226,39 @@ int read_mat(int *m, int *n, int *nzA, FILE* fa, int *is_pattern) {
 }
 
 /*
- * Converts matrix to CSR format. 
- * returns the number of nonZero's found during conversion
- */
-int convert_to_csr(int m, int n, float *A, int *sA_rows, int *sA_col_idx, float *sA_vals) {
-  int i,j; 
-  int checkNZ=0; 
+ *  * Converts matrix to CSC format. 
+ *   * returns the number of nonZero's found during conversion
+ *    */
+int convert_to_csc(int m, int n, float *A, int *sA_cols, int *sA_row_idx, float *sA_vals) {
+  int i,j;
+  int checkNZ=0;
   float tmp;
- 
-  sA_rows[0]=0;
+
+  sA_cols[0]=0;
   checkNZ=0;
-  for (i=0; i<m; i++) {
-    for (j=0; j<n; j++) {
-	tmp = A[i*n+j];
-	if (tmp != 0) {
- 	   sA_col_idx[checkNZ]=j;
-	   sA_vals[checkNZ]=tmp;
-	   checkNZ++; 
-	}	
-    }	
-    sA_rows[i+1]=checkNZ;
+  for (i=0; i<n; i++) {
+    for (j=0; j<m; j++) {
+        tmp = A[j*n+i];
+        if (tmp != 0) {
+           sA_row_idx[checkNZ]=j;
+           sA_vals[checkNZ]=tmp;
+           checkNZ++;
+        }
+    }
+    sA_cols[i+1]=checkNZ;
   }
   return checkNZ;
 }
 
-void print_mat_csr(int m, int nzA, int *sA_rows, int *sA_col_idx, float *sA_vals)
+void print_mat_csc(int n, int nzA, int *sA_cols, int *sA_row_idx, float *sA_vals)
 {
   int i;
-  for (i=0; i<m+1; i++)
-    printf("%d ", sA_rows[i]);
+  for (i=0; i<n+1; i++)
+    printf("%d ", sA_cols[i]);
   printf("\n");
 
   for (i=0; i<nzA; i++)
-    printf("%d ", sA_col_idx[i]);
+    printf("%d ", sA_row_idx[i]);
 
   printf("\n");
 
@@ -275,7 +272,7 @@ void print_mat_csr(int m, int nzA, int *sA_rows, int *sA_col_idx, float *sA_vals
 int main (int argc, char** argv) {
  float *A, *B, *C;
  float *sA_vals;
- int *sA_rows, *sA_cols_idx;
+ int *sA_cols, *sA_rows_idx;
 
  int m, n, err;
  int nzA=0, is_pattern = 1;
@@ -312,14 +309,14 @@ int main (int argc, char** argv) {
 	read_dense(fa, m,n, A);
    fclose(fa);
 
-        sA_rows = (int *)calloc(m+1,sizeof(int));
-        sA_cols_idx = (int *)calloc(nzA,sizeof(int));
+        sA_cols = (int *)calloc(n+1,sizeof(int));
+        sA_rows_idx = (int *)calloc(nzA,sizeof(int));
         sA_vals = (float *)calloc(nzA,sizeof(float));
 
-        convert_to_csr(m,n, A, sA_rows, sA_cols_idx, sA_vals);
+        convert_to_csc(m,n, A, sA_cols, sA_rows_idx, sA_vals);
 #ifdef VERBOSE
-	print_mat(m,n,A);	
- 	print_mat_csr(m, nzA, sA_rows, sA_cols_idx, sA_vals);
+        print_mat(m,n,A);
+        print_mat_csc(m, nzA, sA_cols, sA_rows_idx, sA_vals);
 #endif
 
 #endif
@@ -336,7 +333,7 @@ int main (int argc, char** argv) {
  if (C==NULL) {printf("Out of memory C! \n"); exit(1);}
 
  /* Call the SpMV kernel. */
-  csr_spmv(m, n, nzA, sA_rows, sA_cols_idx, sA_vals, B, C);
+  csc_spmv(m, n, nzA, sA_cols, sA_rows_idx, sA_vals, B, C);
 
  if ((fc = fopen(argv[2], "wt")) == NULL) exit(3);    
 // write_sparse(fc,n,m,C);
